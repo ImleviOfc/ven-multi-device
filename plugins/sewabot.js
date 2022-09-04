b3b434891a6b8bc842dbb9d37d861b2935a9fd4b
levi <111189476+ImleviOfc@users.noreply.github.com>
2022-09-04 08:54:46 +0800

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -3,16 +3,17 @@ let handler = async (m, { conn }) => {
   let pepe = 'https://telegra.ph/file/2e2eb7d093fec08fd00d4.jpg'
   let baper = await fetch(pepe).then(a => a.buffer())
 
-  let arietube = '6285828357727@s.whatsapp.net'
+  let arietube = '6285172446692@s.whatsapp.net'
   let a = await conn.profilePictureUrl(conn.user.jid, 'image').catch((_) => "https://telegra.ph/file/057b8dff2f6ba7d4553ae.jpg")
   let b = await conn.profilePictureUrl(owner[0]+'@s.whatsapp.net', 'image').catch((_) => "https://telegra.ph/file/057b8dff2f6ba7d4553ae.jpg")
   let c = pickRandom([a, b])
   let d = await fetch(c).then(a => a.buffer())
   let prepare = await require('@adiwajshing/baileys').generateWAMessageFromContent(m.key.remoteJid,{listMessage:{
   title: `${await conn.getName(conn.user.jid)}`,
-  description: ` *𝐉𝐀𝐒𝐀 𝐑𝐔𝐍 𝐁𝐎𝐓 𝐓𝐈𝐍𝐆𝐆𝐀𝐋 𝐒𝐂𝐀𝐍\n𝐌𝐈𝐍𝐀𝐓? 𝐂𝐇𝐀𝐓 𝐖𝐀 𝐃𝐈𝐁𝐀𝐖𝐀𝐇*
+  description: ` *HAI KAK INGIN SEWA BOT? SILAHKAN HUBUNGI OWNER*
+  
 
-☞𝑜𝑤𝑛𝑒𝑟 ❀wa.me/${owner[0]}✵
+☞𝑜𝑤𝑛𝑒𝑟 ❀wa.me/${owner[0]}♨️
 
 *Owner ${conn.user.name}*
 `,
@@ -24,7 +25,7 @@ let handler = async (m, { conn }) => {
   products:[{productId:'7487741964584083'}]}],
   headerImage: { productId: '7487741964584083',
   jpegThumbnail: baper },
-  businessOwnerJid: `6285828357727@s.whatsapp.net`
+  businessOwnerJid: `6285172446692@s.whatsapp.net`
   },
   footerText: '',
   }},{})
